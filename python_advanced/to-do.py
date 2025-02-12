c95d568328709d8d6ac7f5f8dd41e41e5ae4a80f
vectorleo <onwukavictorleo@gmail.com>
2025-02-12 01:17:19 +0100

    minnor changes

--- python_advanced/to-do.py
+++ python_advanced/to-do.py
@@ -5,24 +5,46 @@
 file = 'to-do.txt'
 
 def add_task(file):
-  with open(file,'a+') as data_base:
-    task = input('Enter a task: ')
-    if data_base.readlines() == None:
-      data_base.write(f"{task} , incomplete")
+  task = input('Enter a task: ')
+  with open(file,'r') as data_base:
+    if data_base.read(1) == '':
+      with open(file,'a') as data_base:
+        data_base.write(f"{task} , incomplete")
     else:
-      data_base.write(f"\n{task} , incomplete")
+      with open(file,'a') as data_base:
+        data_base.write(f"\n{task} , incomplete")
     
 
-def update_task():
-  pass
-def delete():
-  pass
+def update_task(file):
+  view(file)
+  num = input("Enter the number of the task compeleted: ")
+  with open(file, 'r') as data_base:
+    lines = data_base.readlines()
+    lines[num] = 'kdfl'
+
+def delete_task(file):
+  view(file)
+  action = int(input('Enter the number of the line to delete: '))
+  with open(file,'r') as data_base:
+    line = data_base.readlines()
+
+  del line[action - 1]
+
+  with open(file,'w') as data_base:
+    data_base.write(file)
+  
+  print('Task deleted.')
+
+
 def view(file):
   print('-----------to-do-----------')
   with open(file, 'r') as data_base:
-   print(data_base.readline())
+   num = 0
+   for i in data_base.readlines():
+     num += 1
+     print(num,')',i)
   print('---------------------------')
 
 add_task(file)
 view(file)
-
+delete_task(file)
