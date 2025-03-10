7fb55f907cbf107abd61d5d787e0f1be994256fb
vectorleo <onwukavictorleo@gmail.com>
2025-03-10 11:53:50 +0100

    adding a programme

---
+++ python_advanced/inventory/inventory.py
@@ -0,0 +1,10 @@
+# an invetory management project with python 
+
+class Product:
+
+  def __init__(self,name,price,quantity):
+    self.name = name
+    self.price = price 
+    self.quantity = quantity
+
+  
